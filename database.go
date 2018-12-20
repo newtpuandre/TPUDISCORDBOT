f24095aeec6d3852232ff97500144f6488babef0
newtpuandre <andgun96@gmail.com>
2018-12-20 15:57:58 +0100

    Updated commands command

--- database.go
+++ database.go
@@ -159,6 +159,9 @@ func addCommands() {
 	addSound.Command = "!github"
 	Info.Commands = append(Info.Commands, addSound)
 
+	addSound.Command = "!upload"
+	Info.Commands = append(Info.Commands, addSound)
+
 	for i := range DBSoundList {
 		var temp CommandRoute
 		if DBSoundList[i].enabled == "1" {
