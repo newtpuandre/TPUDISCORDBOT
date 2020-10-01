f2019b609fef5147a75b75e8cf56eb1644da3c41
newtpuandre <andgun96@gmail.com>
2020-10-01 15:17:42 +0200

    Debug is now verbose

--- Program.cs
+++ Program.cs
@@ -18,20 +18,30 @@ namespace TPUDISCORDBOT
         {
             //Read config from app.config
             var appSettings = ConfigurationManager.AppSettings;
-            var test = appSettings.Get("DiscordKey");
-            Console.WriteLine("Read key : " + test);
+            var DiscordToken = appSettings.Get("DiscordKey");
 
 
-            _client = new DiscordSocketClient();
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose
+            });
 
             _client.Log += Log;
+            _client.MessageReceived += MessageReceived;
 
-            await _client.LoginAsync(TokenType.Bot,
-                Environment.GetEnvironmentVariable("DiscordToken"));
+            await _client.LoginAsync(TokenType.Bot, DiscordToken);
             await _client.StartAsync();
 
             // Block this task until the program is closed.
             await Task.Delay(-1);
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content == "!ping")
+            {
+                await message.Channel.SendMessageAsync("Pong!");
+            }
         }
 
         private Task Log(LogMessage msg)
