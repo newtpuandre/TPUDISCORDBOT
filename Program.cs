6c80e4ef0249ce6b4563795fb459c0718f4e8298
newtpuandre <andgun96@gmail.com>
2020-10-01 15:10:49 +0200

    Added some basic discord.net code. Added a config file and a config manager

--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using System.Configuration;
 
 namespace TPUDISCORDBOT
 {
     class Program
     {
-        static void Main(string[] args)
+
+        private DiscordSocketClient _client;
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            //Read config from app.config
+            var appSettings = ConfigurationManager.AppSettings;
+            var test = appSettings.Get("DiscordKey");
+            Console.WriteLine("Read key : " + test);
+
+
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot,
+                Environment.GetEnvironmentVariable("DiscordToken"));
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
