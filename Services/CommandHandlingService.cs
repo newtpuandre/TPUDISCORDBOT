388dba5fa0551395f247dac242228a5a33d67e0b
newtpuandre <andgun96@gmail.com>
2020-10-09 14:00:27 +0200

    Removed unused using statements

--- Services/CommandHandlingService.cs
+++ Services/CommandHandlingService.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Reflection;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection.Abstractions;
 using Microsoft.Extensions.DependencyInjection;
 using Discord;
 using Discord.Commands;
